82c037d1a0edcf21047a7c24f43d22bf7059fae7
jiroyamada <poirot343@outlook.jp>
2021-08-01 11:49:51 +0900

    estimate_meansクラス

--- ch1/bandit01.py
+++ ch1/bandit01.py
@@ -1,4 +1,36 @@
 # -*- coding: utf-8 -*-
 
 
-a
+import numpy as np
+from pandas import DataFrame
+import matplotlib
+matplotlib.rcParams['font.size'] = 12
+
+class Bandit:
+    def __init__(self, arms = 10):
+        self.arms = arms
+        self.means = np.random.normal(loc = 0.0, scale = 1.0, size = self.arms)
+        
+    def select(self, arm):
+        reward = np.random.normal(loc = self.means[arm], scale = 1.0)
+        return reward
+        '''
+        print(reward)
+        print(arm)
+        '''
+        
+def estimate_means(bandit,steps):
+    rewards = {}
+    for arm in range(bandit.arms):
+        rewards[arm] = []
+    
+    for _ in range(steps):
+        arm = np.random.randint(bandit.arms)
+        reward = bandit.select(arm)
+        rewards[arm].append(reward)
+        
+    averages = []
+    for arm in range(bandit.arms):
+        averages.append(sum(rewards[arm])/len(rewards[arm]))
+    
+    return averages
